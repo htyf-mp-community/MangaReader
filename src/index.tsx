219d747271f4c6299f886185ddd9dfc5dcedc1d5
dgz <dagouzhi@qq.com>
2026-06-01 18:32:18 +0800

    feat: ad

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import '@/utils/define';
 import { BackHandler, LogBox } from 'react-native';
 import { CacheManager } from '@georstat/react-native-image-cache';
 import { Dirs } from 'react-native-file-access';
+import jssdk from '@htyf-mp/js-sdk';
+import { useEffect } from 'react';
 import App from './App';
 import packageJson from '../package.json';
 
@@ -31,5 +33,34 @@ CacheManager.config = {
 
 LogBox.ignoreLogs(['Require cycle:', 'Remote debugger']);
 
-export default App;
+export default () => {
+  useEffect(() => {
+    const tryShowInterstitialAd = async () => {
+      const INTERSTITIAL_AD_STORAGE_KEY = 'interstitial_ad_last_shown_at';
+      /** 冷却时间（毫秒），此时间内只调用一次 showInterstitialAd */
+      const INTERSTITIAL_AD_COOLDOWN_MS = 8 * 60 * 1000;
+      const storage = jssdk.getStorage();
+      const lastShownRaw = await storage.getItem(INTERSTITIAL_AD_STORAGE_KEY);
+      const now = Date.now();
+      const lastShown = lastShownRaw ? Number(lastShownRaw) : 0;
+
+      if (lastShown && now - lastShown < INTERSTITIAL_AD_COOLDOWN_MS) {
+        return;
+      }
+
+      await storage.setItem(INTERSTITIAL_AD_STORAGE_KEY, String(now));
+      jssdk.showInterstitialAd({
+        onOpen: () => {
+          console.log('onOpen');
+        },
+        onClose: () => {
+          console.log('onClose');
+        },
+      });
+    };
+
+    tryShowInterstitialAd();
+  }, []);
+  return <App />;
+}
 
