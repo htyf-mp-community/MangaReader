d9828865361434f82d3bbb24b9fb67b2bab479b3
dgz <dagouzhi@qq.com>
2026-07-08 16:30:27 +0800

    RN 0.86.0

--- react-native.config.js
+++ react-native.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   commands: require('@callstack/repack/commands/webpack'),
-  dependencies: {
-    // Keep @d11/react-native-fast-image only; disable legacy package autolinking.
-    'react-native-fast-image': {
-      platforms: {
-        android: null,
-        ios: null
-      }
-    }
-  }
+  dependencies: {}
 };
